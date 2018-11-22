2df6a710c266b8196bd62de157f8c57af43dd1d2
andreybogan <andreybogan@yandex.ru>
2018-11-22 08:08:07 +0400

    Lesson 4. v.1

--- sql_requests_organization.sql
+++ sql_requests_organization.sql
@@ -1,20 +1,21 @@
 USE `organization`;
 
 -- 1. Выбрать среднюю зарплату по отделам.
+CREATE VIEW `avg_salary` AS
 SELECT `d`.`name`, AVG(`e`.`salary`) FROM `employees` `e` LEFT JOIN `departments` `d` ON `e`.`department_id` = `d`.`id` GROUP BY `d`.`id`; 
+SELECT * FROM `avg_salary`;
 
 -- 2. Выбрать максимальную зарплату у сотрудника.
-SELECT `name`, `salary` FROM `employees` ORDER BY `salary` DESC LIMIT 1;
--- или
-SELECT MAX(`salary`) FROM `employees`;
+CREATE VIEW `max_salary` AS
+SELECT MAX(`salary`) FROM `employees` ORDER BY `salary`;
+SELECT * FROM `max_salary`;
 
--- 3. Удалить одного сотрудника, у которого максимальная зарплата.
-DELETE FROM `employees` ORDER BY `salary` DESC LIMIT 1;
--- или 
-DELETE FROM `employees` WHERE `salary` = (SELECT * FROM (SELECT MAX(`salary`) FROM `employees`) AS `temp`);
-
--- 4. Посчитать количество сотрудников во всех отделах.
+-- 3. Посчитать количество сотрудников во всех отделах.
+CREATE VIEW `count_employee_in_department` AS
 SELECT `d`.`name`, COUNT(`e`.`id`) FROM `employees` `e` LEFT JOIN `departments` `d` ON `e`.`department_id` = `d`.`id`  GROUP BY `e`.`department_id`;
+SELECT * FROM `count_employee_in_department`;
 
 -- 4. Найти количество сотрудников в отделах и посмотреть, сколько всего денег получает отдел.
+CREATE VIEW `count_get_money_in_department` AS
 SELECT `d`.`name`, COUNT(`e`.`id`) `count`, SUM(`e`.`salary`) `total` FROM `employees` `e` LEFT JOIN `departments` `d` ON `e`.`department_id` = `d`.`id`  GROUP BY `e`.`department_id`;
+SELECT * FROM `count_get_money_in_department`;
